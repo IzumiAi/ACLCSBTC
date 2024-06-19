dd5c6b87b9d5e6d51c0503f78d13cf9b3ac8c0ce
IzumiAi <edward.louis.pingal@aclcbutuan.edu.ph>
2024-06-19 15:58:55 +0800

    firstactivity

--- Day2/Sir Ian/sbc-d2-act2.py
+++ Day2/Sir Ian/sbc-d2-act2.py
@@ -1,27 +1,27 @@
 import random
 
 def get_choices():
-    return random.randint(0, 1)
+    return random.randint("hayang", "kulob")
 
 def determine_winner(choices):
     if len(set(choices)) == 1:
         return "None, continue the game!."
     else:
         winner = choices.index(min(set(choices), key = choices.count))
-        if winner == 0:
+        if winner == ("hayang"):
             return "Player 1"
-        elif winner == 1:
+        elif winner == ("kulob"):
             return "Computer 2"
         else:
             return "Computer 3"
 
 def play_game():
-    player_choice = int(input())
+    player_choice = int("hayang or kulob?: ")
     computer_choices = [get_choices() for _ in range(2)]
     choices = [player_choice] + computer_choices
-    print("Player 1 choose: " + ("Fold" if player_choice == 0 else "Unfold"))
-    print("Computer 2 choose: " + ("Fold" if computer_choices[0] == 0 else "Unfold"))         
-    print("Computer 3 choose: " + ("Fold" if computer_choices[0] == 0 else "Unfold")) 
+    print("Player 1 choose: " + ("Fold" if player_choice == "hayang" else "Unfold"))
+    print("Computer 2 choose: " + ("Fold" if computer_choices["hayang"] == "hayang" else "Unfold"))         
+    print("Computer 3 choose: " + ("Fold" if computer_choices["hayang"] == "hayang" else "Unfold")) 
     winner = determine_winner(choices)
     if winner == "None":
         print(winner + " wins! balaa gud nimo")
