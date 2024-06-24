d4946b6445a063c1210ff7add162daa5308e016b
IzumiAi <edward.louis.pingal@aclcbutuan.edu.ph>
2024-06-24 14:48:11 +0800

    day5act1

---
+++ sbc-d5-act.py
@@ -0,0 +1,20 @@
+commands = []
+while True:
+    command = input("COMMAND: ")
+    if command.startswith("3"):
+        num = command.split()[-1]
+        commands.append(num)
+        print(commands)
+    elif command == "NAA":
+        if commands:
+            commands.pop(0)
+        print(commands)
+    elif command == "WALA":
+        if commands:
+            commands.pop()   
+            print(commands) 
+    elif command == "DISPLAY":
+        print(commands)
+    else:
+        commands.append(command)
+        print(commands)
